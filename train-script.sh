6c28733a4b7bbc45ed638e294db7df5d7fe737d8
Tingyao Hsu <txh357@lrs-khuang01.ist.psu.edu>
2023-07-18 10:26:17 -0400

    update baseline code & training script

--- train-script.sh
+++ train-script.sh
@@ -1,16 +1,15 @@
-CUDA_VISIBLE_DEVICES=0 python3 scicap_sum_baseline.py \
-    --num_train_epochs 1 \
+python3 scicap_sum_baseline.py \
+    --num_train_epochs 3 \
     --model_name_or_path google/pegasus-arxiv \
     --output_dir baseline \
-    --per_device_train_batch_size=16 \
-    --per_device_eval_batch_size=1 \
+    --per_device_train_batch_size=32 \
+    --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
     --train_file split_json_data_dir/train.json \
     --validation_file split_json_data_dir/val.json \
     --test_file split_json_data_dir/test.json \
     --dataset_name scicap_summary \
-    --sortish_sampler \
-    --max_source_length 256 \
+    --max_source_length 512 \
     --max_target_length 100 \
     --load_best_model_at_end \
     --evaluation_strategy="epoch" \
@@ -19,12 +18,9 @@ CUDA_VISIBLE_DEVICES=0 python3 scicap_sum_baseline.py \
     --learning_rate=5e-5 \
     --do_train \
     --do_eval \
-    --text_column paragraph \
-    --summary_column caption_no_index \
-    --max_train_samples 100 \
-    --max_eval_samples 10 \
-    --max_predict_samples 10 \
-    --sortish_sampler \
-    --auto_find_batch_size \
     --do_predict \
     --predict_with_generate \
+    --text_column paragraph \
+    --summary_column caption_no_index \
+    --sortish_sampler \
+    --auto_find_batch_size \
